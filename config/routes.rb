928abbd04fde33f6d34681987d53feba82f50612
Franco <francoezapata1@gmail.com>
2021-11-05 21:52:47 -0300

    Proyecto Final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :personajes
-  resources :peliculas
-  resources :generos
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :personajes
+      resources :peliculas
+      resources :generos
+    end
+  end
 end
