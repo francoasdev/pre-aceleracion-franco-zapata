9ad4bcac86b98bce6134e6604de25b8c5ff2cdcd
Franco <francoezapata1@gmail.com>
2021-11-08 20:50:46 -0300

    Proyecto final

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users,
+             controllers: {
+                 sessions: 'api/v1/users/sessions',
+                 registrations: 'api/v1/users/registrations'
+             }
+  get '/member-data', to: 'members#show'
   namespace :api do
     namespace :v1 do
+
       resources :personajes
       resources :peliculas
       resources :generos
